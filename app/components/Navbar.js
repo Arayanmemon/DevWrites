e91fbe302720535ba797489ec6e65e22e72fb860
Arayanmemon <memonaryu@gmail.com>
2023-07-29 12:29:26 +0500

    Home page setup

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <div className='bg-purple-300'>
+        <div className='bg-purple-400'>
             <header class="text-gray-600 body-font">
                 <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
@@ -12,10 +12,10 @@ export default function Navbar() {
                         <span class="ml-3 text-xl">DevWrites</span>
                     </a>
                     <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                        <a class="mr-5 hover:text-gray-900">First Link</a>
-                        <a class="mr-5 hover:text-gray-900">Second Link</a>
-                        <a class="mr-5 hover:text-gray-900">Third Link</a>
-                        <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+                        <a class="mr-5 hover:text-gray-900" href='/'>Home</a>
+                        <a class="mr-5 hover:text-gray-900" href='/'>Categories</a>
+                        <a class="mr-5 hover:text-gray-900" href='/'>About us</a>
+                        <a class="mr-5 hover:text-gray-900" href='/'>Contact us</a>
                     </nav>
                     <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
--- app/page.js
+++ app/page.js
@@ -1,20 +1,38 @@
 'use client'
-import Image from 'next/image'
-import { Client } from "appwrite";
+import React, { useEffect, useState } from 'react';
+import { Client, Databases } from 'appwrite';
 import Navbar from './components/Navbar';
-import PostSection from './components/Posts';
+import Posts from './components/Posts';
+import Loader from "./components/Loader";
 const client = new Client();
 
 client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('64b57c98055fabedae12');
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject('64b57c98055fabedae12');
 
+const databases = new Databases(client);
 
 export default function Home() {
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    const promise = databases.listDocuments('64b6b6eb0e8606bcce13', '64b6b6f66fbb4fd68725');
+
+    promise
+      .then((result) => {
+        if (result.documents.length > 0) {
+          setResponse(result.documents);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className=''>
-    <Navbar />
-    <PostSection />
+      <Navbar />
+      {response ? <Posts response={response} /> : <Loader />}
     </div>
   );
 }
