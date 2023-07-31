dfd39cfe25e2c19eb76c8ff07a9de9ad23658e20
Arayanmemon <memonaryu@gmail.com>
2023-07-31 13:10:12 +0500

    Post images configuration a

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+        domains: ['encrypted-tbn0.gstatic.com', 'miro.medium.com'], // Add the hostname of the image source here
+    },
+}
